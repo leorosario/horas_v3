ea524c44b1ce93d9bc31e51081c5dc4bc2353aec
Leonardo Barbosa Silva do Rosário <leorosario9605@gmail.com>
2025-12-30 16:31:27 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:horas_v3/components/screens/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
